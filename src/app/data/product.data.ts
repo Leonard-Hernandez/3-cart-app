893db57db8fce9108e82533d99fb62ffd49aee48
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-11-06 14:22:40 -0500

    creando la data y el servicio de productos

---
+++ src/app/data/product.data.ts
@@ -0,0 +1,40 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: 'teclado mecanico asus rgb',
+        description: 'cherry red',
+        price: 1000
+    },
+    {
+        id: 2,
+        name: 'mouse shark x6',
+        description: 'el mejor calidad precion',
+        price: 1600
+    },
+    {
+        id: 3,
+        name: 'mause pad',
+        description: 'super deslizante',
+        price: 500
+    },
+    {
+        id: 4,
+        name: 'monitor asus qhd 240hz ips',
+        description: 'garantia extendida',
+        price: 2000
+    },
+    {
+        id: 5,
+        name: 'rtx 4090 ti',
+        description: 'simplemente la mejor de su gama',
+        price: 7000
+    },
+    {
+        id: 6,
+        name: 'Intel Ultra 5',
+        description: 'Menor temperatura, menor consumo y el mismo rendimiento',
+        price: 4000
+    }
+]
