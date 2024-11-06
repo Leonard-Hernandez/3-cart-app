f0bcc482c5ea2e056cff89caab353500ff005146
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-11-06 17:47:07 -0500

    agregando la opcion de eliminar items y el total

--- src/app/components/card-app.component.ts
+++ src/app/components/card-app.component.ts
@@ -17,14 +17,17 @@ export class CartAppComponent implements OnInit {
 
   items: CartItem[] = [];
 
+  total: number = 0;
+
   constructor(private service:ProductService){}
 
   ngOnInit(): void {
     this.products = this.service.findAll();
+    this.calculateTotal();
   }
 
 
-  onAddCart(product: Product){
+  onAddCart(product: Product):void {
     const hasItem = this.items.find(item => {
       return item.product.id === product.id
     })
@@ -43,7 +46,16 @@ export class CartAppComponent implements OnInit {
     } else{
       this.items = [... this.items, {product: {... product}, quantity: 1}]
     }
-    
+    this.calculateTotal()
+  }
+
+  onDeleteCart(id:number):void {
+    this.items = this.items.filter( item => item.product.id !== id)
+    this.calculateTotal();
+  }
+
+  calculateTotal(): void{
+    this.total = this.items.reduce( (accumulator, item) => accumulator + (item.quantity * item.product.price),0)
   }
 
 }
--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItem } from '../../models/cartItem';
 
 @Component({
@@ -11,6 +11,12 @@ export class CartComponent {
 
   @Input() items: CartItem[] = [];
 
+  @Input() total: number = 0;
 
+  @Output() idProductEventEmitter: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCart(id: number){
+    this.idProductEventEmitter.emit(id);
+  }
 
 }
