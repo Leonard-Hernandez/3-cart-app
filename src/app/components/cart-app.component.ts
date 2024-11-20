59b19c9dcf151a501fb204b456a22bd3b2aadd08
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-11-19 22:36:54 -0500

    migracion a redux, creando actions y reducer

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -37,24 +37,7 @@ export class CartAppComponent implements OnInit {
 
   onAddCart():void {
     this.sharingDataService.productEventEmitter.subscribe(product =>{
-      const hasItem = this.items.find(item => {
-        return item.product.id === product.id
-      })
-  
-      if (hasItem) {
-  
-        this.items = this.items.map(item => {
-          if(item.product.id === product.id){
-            return {
-              ... item, quantity: item.quantity +1
-            }
-          }
-          return item;
-        })
-        
-      } else{
-        this.items = [... this.items, {product: {... product}, quantity: 1}]
-      }
+      
       this.calculateTotal()
       this.saveSession();
       this.router.navigate(['/cart'], {
@@ -84,8 +67,6 @@ export class CartAppComponent implements OnInit {
         confirmButtonText: "Yes, delete it!"
       }).then((result) => {
         if (result.isConfirmed) {
-
-          this.items = this.items.filter( item => item.product.id !== id)
           if(this.items.length == 0){
             sessionStorage.removeItem('cart')
           }
